45cf4a82ea0442a855bddef707ce150498c125f3
M.A. Khan <amaank43@gmail.com>
2022-07-25 01:06:48 +0530

    Update Hello user Q.py

--- Devsnest June'22/String/Hello user Q.py
+++ Devsnest June'22/String/Hello user Q.py
@@ -1,1 +1,15 @@
+'''
+You are given a String str. Return the String by Concatenating it with String "Hello, " and an exclamation mark at the end “!”.
 
+Example 1:
+Input:
+    "Mac"
+Output:
+    "Hello, Mac!"
+Explanation:
+    Given a String, Mac is Concatenated with string "Hello, " at the beginning and "!" at the end
+
+Code:
+def solve(str):
+   # CODE HERE    
+'''
