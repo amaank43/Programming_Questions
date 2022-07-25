42d5141ca96b4b340e61a72b2f5ebfdd26cef463
M.A. Khan <amaank43@gmail.com>
2022-07-26 03:57:13 +0530

    Create Removing All Adjacent Dupicates In A String S.py

---
+++ Devsnest June'22/String/Removing All Adjacent Dupicates In A String S.py
@@ -0,0 +1,9 @@
+def solve(s):
+    # CODE HERE
+    stack = []
+    for i in s:
+        if stack and stack[-1] == i:
+            stack.pop()
+        else:
+            stack.append(i)
+    return ''.join(stack)
