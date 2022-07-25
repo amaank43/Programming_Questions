376d2131536334734612919e2cf7f2d6cf271ff4
M.A. Khan <amaank43@gmail.com>
2022-07-26 03:48:18 +0530

    Create Removing All Adjacent Dupicates In A String Q.py

---
+++ Devsnest June'22/String/Removing All Adjacent Dupicates In A String Q.py
@@ -0,0 +1,32 @@
+'''
+Given a string s which consists of lowercase English letters. Removing duplicates means choosing two adjacent and equal letters and removing them.
+
+We repeatedly make duplicate removals on s until we no longer can.
+
+Return the final string after all such duplicate removals have been made.(Answer is proven as unique)
+
+Input Format
+First Parameter - String s
+
+Output Format
+Return the string
+
+Example 1:
+Input: s = "abbaca"
+Output: "ca"
+Explanation: From the "abbaca" remove bb then the result is aaca then 
+again remove aa which makes the remaing string as "ca".
+Example 2:
+Input: s = "azxxzy"
+Output: "ay"
+Constraints:
+1 <= s.length <= 105
+s consists of lowercase English letters.
+Time complexity: O(n)
+Auxillary space complexity: O(n)
+'''
+
+'''
+def solve(s):
+    # CODE HERE
+'''
