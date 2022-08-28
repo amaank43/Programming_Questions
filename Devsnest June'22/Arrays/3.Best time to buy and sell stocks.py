73a6227d9ce567c576afe73a6f86dd0d7972bd6d
M.A. Khan <amaank43@gmail.com>
2022-08-28 18:23:17 +0530

    Create 3.Best time to buy and sell stocks.py

---
+++ Devsnest June'22/Arrays/3.Best time to buy and sell stocks.py
@@ -0,0 +1,12 @@
+# https://devsnest.in/algo-challenges/best-time-to-buy-and-sell-stock?tab=question
+
+def solve(n, prices):
+    # CODE HERE
+    buy = prices[0]
+    max_profit = 0
+    for i in range(1, n):
+        if (buy > prices[i]):
+            buy = prices[i]
+        elif (prices[i] - buy > max_profit):
+            max_profit = prices[i] - buy;
+    return max_profit;
