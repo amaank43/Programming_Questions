0fdba50b10ae914f8ceade4da0151187d632574d
M.A. Khan <amaank43@gmail.com>
2022-10-14 21:19:52 +0530

    Create 3sum S.py

---
+++ Devsnest June'22/Arrays/3sum S.py
@@ -0,0 +1,26 @@
+'''
+https://devsnest.in/algo-challenges/3sum?tab=question
+'''
+
+def solve(n, nums):
+    # CODE HERE
+        ans = []
+        nums.sort()
+        i = 0
+        j,k = i+1,len(nums)-1
+        while i <= len(nums)-3 :
+            if nums[i]+nums[j]+nums[k] > 0 :
+                k-=1
+            elif nums[i]+nums[j]+nums[k] < 0 : 
+                j+=1
+            else :
+                ans.append([nums[i],nums[j],nums[k]])
+                j+=1
+                while nums[j-1]==nums[j] and j<k:
+                    j+=1
+            if j==k:
+                i +=1
+                while nums[i-1]==nums[i] and i <= len(nums)-3:
+                    i+=1
+                j,k = i+1,len(nums)-1
+        return ans   
