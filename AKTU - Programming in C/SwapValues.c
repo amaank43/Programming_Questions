be21072689693a75fc0080a664ca1e129fd710cc
M.A. Khan <amaank43@gmail.com>
2022-11-08 21:16:16 +0530

    Create SwapValues.c

---
+++ AKTU - Programming in C/SwapValues.c
@@ -0,0 +1,17 @@
+//WAP to swap the values of two variables without using third variable
+
+#include<stdio.h>  
+int main()   
+{   
+  int x, y;  
+  printf("Enter the value of x and y?");  
+  scanf("%d %d",&x,&y);  
+  printf("before swapping numbers: %d   %d\n",x,y);  
+  
+/*swapping*/  
+  x = x + y;   
+  y = x - y;    
+  x = x - y;  
+  printf("After swapping: %d    %d",x,y);   
+  return 0;   
+}  
