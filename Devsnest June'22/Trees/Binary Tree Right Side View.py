388638f75b3834e7bab3ed7cc6ebeaa864bb04f4
M.A. Khan <amaank43@gmail.com>
2023-01-01 23:52:03 +0530

    Update Binary Tree Right Side View.py

--- Devsnest June'22/Trees/Binary Tree Right Side View.py
+++ Devsnest June'22/Trees/Binary Tree Right Side View.py
@@ -1,3 +1,19 @@
 '''
 https://devsnest.in/algo-challenges/binary-tree-right-side-view?tab=question
 '''
+def solve(root):
+    # CODE HERE
+        res=[]
+        if root==None:
+            return res
+        q=[]
+        q.append(root)
+        while(len(q)):
+            res.append(q[len(q)-1].val)
+            for i in range(len(q)):
+                temp=q.pop(0)
+                if temp.left:
+                    q.append(temp.left)
+                if temp.right:
+                    q.append(temp.right)
+        return res
