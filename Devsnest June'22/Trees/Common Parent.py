a15b80287f1df7dc5966d57d50978664325d85de
M.A. Khan <amaank43@gmail.com>
2023-01-01 23:55:56 +0530

    Update Common Parent.py

--- Devsnest June'22/Trees/Common Parent.py
+++ Devsnest June'22/Trees/Common Parent.py
@@ -2,3 +2,14 @@
 https://devsnest.in/algo-challenges/common-parent?tab=question
 '''
 
+def solve(root, p, q):
+    # CODE HERE
+    if root is None or root.val in {p,q}:
+
+        return root
+    
+    left = solve(root.left,p,q) 
+
+    right = solve(root.right,p,q)
+    
+    return root if left and right else left or right
