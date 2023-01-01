97e8ce0a4adf86c07cc42c3523e68eca8d901ac8
M.A. Khan <amaank43@gmail.com>
2023-01-01 23:56:52 +0530

    Update Merge Two Binary Trees.py

--- Devsnest June'22/Trees/Merge Two Binary Trees.py
+++ Devsnest June'22/Trees/Merge Two Binary Trees.py
@@ -1,3 +1,13 @@
 '''
 https://devsnest.in/algo-challenges/merge-two-binary-trees?tab=question
 '''
+def solve(root1, root2):
+    # CODE HERE
+    if not root1:
+        return root2
+    if not root2:
+        return root1
+    root1.val += root2.val
+    root1.left = solve(root1.left,root2.left)
+    root1.right = solve(root1.right, root2.right)
+    return root1
