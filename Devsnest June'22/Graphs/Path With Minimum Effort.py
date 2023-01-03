9ea6cc15529c38b8f012fb60713e2a04c890d2e3
M.A. Khan <amaank43@gmail.com>
2023-01-03 18:19:44 +0530

    Update Path With Minimum Effort.py

--- Devsnest June'22/Graphs/Path With Minimum Effort.py
+++ Devsnest June'22/Graphs/Path With Minimum Effort.py
@@ -2,3 +2,23 @@
 https://devsnest.in/algo-challenges/path-with-minimum-effort?tab=question
 
 '''
+import heapq
+def solve(height):
+    # CODE HERE
+        m = len(height)
+        n = len(height[0])
+        vis = [[False for i in range(n)] for j in range(m)]
+        q = []
+        heapq.heappush(q, (0,0,0))
+        while len(q) > 0:
+            cur = heapq.heappop(q)
+            if vis[cur[1]][cur[2]]:
+                continue
+            if cur[1] == m-1 and cur[2] == n-1:
+                return cur[0]
+            vis[cur[1]][cur[2]] = True
+            for _x, _y in [(-1,0),(1,0),(0,-1),(0,1)]:
+                x, y = cur[1]+_x, cur[2]+_y
+                if 0<=x<m and 0<=y<n and not vis[x][y]:
+                    heapq.heappush(q, (max(cur[0], abs(height[x][y]-height[cur[1]][cur[2]])), x, y))
+        return -1
