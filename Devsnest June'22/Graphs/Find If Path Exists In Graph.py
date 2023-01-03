2164d46efafefcbb4e6ac53a65072f54cb6682b2
M.A. Khan <amaank43@gmail.com>
2023-01-03 18:19:07 +0530

    Update Find If Path Exists In Graph.py

--- Devsnest June'22/Graphs/Find If Path Exists In Graph.py
+++ Devsnest June'22/Graphs/Find If Path Exists In Graph.py
@@ -2,3 +2,28 @@
 https://devsnest.in/algo-challenges/find-if-path-exists-in-graph?tab=question
 
 '''
+def solve(n, edges, source, destination):
+    # CODE HERE
+        if source == destination:
+            return 1
+        graph = dict();
+        
+        for i in range(n):
+            graph[i] = []
+        for edge in edges:
+            graph[edge[0]].append(edge[1])
+            graph[edge[1]].append(edge[0])
+            
+        visited = [0]*n
+        dq = deque([source])
+        
+        while dq:
+            node = dq.popleft()
+            
+            for nbr in graph[node]:
+                if nbr == destination:
+                    return 1
+                elif not visited[nbr]:
+                    visited[nbr] = 1
+                    dq.append(nbr)
+        return 0
