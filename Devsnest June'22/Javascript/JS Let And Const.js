67083086e1f68e9e22a18dd705aaf8570e1e7e22
M.A. Khan <amaank43@gmail.com>
2023-01-05 11:20:55 +0530

    Create JS Let And Const.js

---
+++ Devsnest June'22/Javascript/JS Let And Const.js
@@ -0,0 +1,17 @@
+<!--https://devsnest.in/frontend-challenges/javascript/new-js-challenge/170-->
+
+{
+    // Write your from here
+    var name;
+    let age;
+    let isEmployeed;
+    const URL= 'http://localhost:3000' ;
+    name = 'Aman';
+  age = 22;
+  isEmployeed =false;
+    // to here
+
+  
+  // dont remove this line
+  try { module.exports = { name, age, isEmployeed, URL } } catch (e) { }
+}
