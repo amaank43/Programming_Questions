91e3f818ca9d258fe8efb20c843e4b1210b1eaed
M.A. Khan <amaank43@gmail.com>
2023-01-07 14:33:37 +0530

    Create JS Arrow Functions.js

---
+++ Devsnest June'22/Javascript/JS Arrow Functions.js
@@ -0,0 +1,12 @@
+<!--https://devsnest.in/frontend-challenges/javascript/js-arrow-functions/194-->
+
+
+// Create an arrow function devsnest 
+// that returns a string `Hello World!`
+// Note: don't use function and explicit return keywords
+
+
+// code here
+let devsnest = () => "Hello World!";
+
+try { module.exports = { devsnest } } catch(e) {}
