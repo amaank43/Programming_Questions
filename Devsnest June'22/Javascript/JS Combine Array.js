d492dbfc68d98a5ef6d75ad433aa559e158059ea
M.A. Khan <amaank43@gmail.com>
2023-01-07 14:33:10 +0530

    Create JS Combine Array.js

---
+++ Devsnest June'22/Javascript/JS Combine Array.js
@@ -0,0 +1,16 @@
+<!--https://devsnest.in/frontend-challenges/javascript/js-combine-array/199-->
+
+// you're given two arrays arr1 and arr2 in the parameters
+// of the function combine.
+// from the function combine return new array that is
+// combination of these two arrays
+// All elements of arr1 should come before arr2's elements
+// Suggestion: use Spread Syntax
+
+function combine(arr1, arr2) {
+	// code here	
+	return arr1 = [...arr1, ...arr2];
+}
+
+
+try { module.exports = { combine } } catch(e) {}
