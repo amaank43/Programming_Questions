e1081da119946199a4d3a9d8b663fb533ee4d0c7
M.A. Khan <amaank43@gmail.com>
2023-05-02 22:13:58 +0530

    Create JS Get Names.js

---
+++ Devsnest June'22/Javascript/JS Get Names.js
@@ -0,0 +1,16 @@
+<!--https://devsnest.in/frontend-challenges/javascript/js-get-names/210-->
+
+// `persons` is an array of object `person` 
+// (definition of `person` given in instructions)
+// you have to return an array containing names of 
+// persons with salary less than 30000
+// use Array.map() and Array.filter() for this task
+
+
+function getNames(persons) {
+	// code here
+	var ans = persons.filter(person => person.salary<30000);
+    return ans.map(x=> x.name);
+}
+
+try { module.exports = { getNames } } catch(e) {}
